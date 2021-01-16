166976f17b37e021ad31363893ad3f98934f80f7
anas salama <salamaanas681@gmail.com>
2021-01-16 13:20:08 +0200

    Api Resource Test

---
+++ database/factories/CartFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Cart;
+use Faker\Generator as Faker;
+
+$factory->define(Cart::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
